cf98c072b253949c3e0119a10d9ba413acf4e1f0
Julio Faerman <jfaerman@gmail.com>
2021-02-15 23:43:35 +0100

    vai py

--- rom-to-dec/tests/test_module.py
+++ rom-to-dec/tests/test_module.py
@@ -1,4 +1,19 @@
 from romtodec import romanToInt
 
-def test_one():
+def test_1():
     assert romanToInt("I") == 1
+
+def test_39():
+    assert romanToInt("XXXIX") == 39
+
+def test_4():
+    assert romanToInt("IV") == 4
+
+def test_6():
+    assert romanToInt("VI") == 6
+
+def test_499():
+    assert romanToInt("CDXCIX") == 499
+
+def test_3999():
+    assert romanToInt("MMMCMXCIX") == 3999
