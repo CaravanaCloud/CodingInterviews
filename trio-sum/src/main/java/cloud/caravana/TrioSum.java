544b1414cd7fb00d3612b4f0c27da3869e822733
Julio Faerman <jfaerman@gmail.com>
2021-02-19 09:15:32 +0100

    wrong sum :/

--- trio-sum/src/main/java/cloud/caravana/TrioSum.java
+++ trio-sum/src/main/java/cloud/caravana/TrioSum.java
@@ -2,6 +2,28 @@ package cloud.caravana;
 import java.util.*;
 
 class TrioSum {
+
+    // O (n log(n)) time - O(n) space
+    public static List<Integer[]> fastSum( int[] array, int targetSum) {
+        Arrays.sort(array);
+        List<Integer[]> result = new ArrayList<>();
+        for (int i= 0; i < array.length - 2; i++) {
+            int left = i + 1;
+            int right = array.length - 1;
+            while (left < right) {
+                int currentSum = array[i] + array[left] + array[right];
+                if (currentSum == targetSum) {
+                    result.add(new Integer[]{array[i], array[left], array[right]});
+                    left++;
+                    right--;
+                } else if (currentSum < targetSum)
+                    left++;
+                else right--;
+            }
+        }
+        return  result;
+    }
+
     // O(n^3) time - O(n) space
     public static List<Integer[]> naiveSum(int[] xs, int targetSum) {
         List<Integer[]> result = new ArrayList<>();
--- trio-sum/src/test/java/cloud/caravana/TrioSumTest.java
+++ trio-sum/src/test/java/cloud/caravana/TrioSumTest.java
@@ -14,32 +14,44 @@ public class TrioSumTest {
     Integer[] e1 = new Integer[]{-8, 3, 5};
     Integer[] e2 = new Integer[]{-6, 1, 5};
 
-
-    int[] xxs = null;
-    int xxs_size = 333;
     int xxs_target = 42;
 
+    @Test
+    public void testSimpleFastSum() {
+        List<Integer[]> result = TrioSum.fastSum(xs.clone(),xs_target);
+        assertArrayEquals(result.get(0), e0);
+        assertArrayEquals(result.get(1), e1);
+        assertArrayEquals(result.get(2), e2);
+    }
+
+    @Test
+    public void testHardFastSum() {
+        List<Integer[]> result = TrioSum.fastSum(randomArray(0,333),xxs_target);
+        assertEquals(6911, result.size());
+    }
 
     @Test
     public void testSimpleNaiveSum() {
-        List<Integer[]> result = TrioSum.naiveSum(xs,xs_target);
-        Integer[] r0 = result.get(0);
-        Integer[] r1 = result.get(1);
-        Integer[] r2 = result.get(2);
-        assertArrayEquals(r0, e0);
-        assertArrayEquals(r1, e1);
-        assertArrayEquals(r2, e2);
+        List<Integer[]> result = TrioSum.naiveSum(xs.clone(),xs_target);
+        assertArrayEquals(result.get(0), e0);
+        assertArrayEquals(result.get(1), e1);
+        assertArrayEquals(result.get(2), e2);
     }
 
     @Test
     public void testHardNaiveSum() {
-        Random rand = new Random(0);
-        xxs = new int[xxs_size];
-        for (int i = 0; i < xxs_size ; i++) {
-            xxs[i] = (rand.nextBoolean() ? 1 : -1) * rand.nextInt(xxs_size);
-        }
-        List<Integer[]> result = TrioSum.naiveSum(xxs,xxs_target);
+        List<Integer[]> result = TrioSum.naiveSum(randomArray(0,333),xxs_target);
         assertEquals(6911, result.size());
+    }
+
+    private static int[] randomArray(int seed, int size){
+        int[] xxs = null;
+        Random rand = new Random(seed);
+        xxs = new int[size];
+        for (int i = 0; i < size ; i++) {
+            xxs[i] = (rand.nextBoolean() ? 1 : -1) * rand.nextInt(size);
+        }
+        return xxs;
     }
 
     private void print(List<Integer[]> xxs) {
